340a7e7b6895f215ef7af810c9c07cf12ae2838e
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-04 13:29:07 -0500

    NumberOfLikesBtns added

---
+++ client.js
@@ -0,0 +1,35 @@
+console.log('client ready');
+$(document).ready (function(){
+    clickHandlers();    
+});
+
+function clickHandlers(){
+    console.log('jQ ready');
+    $('.changeLikesBox').on('click','#plusBtn',addLike)
+    $('.changeLikesBox').on('click','#minusBtn',removeLike)
+}
+
+let numberLikes = 0;
+
+function addLike() {
+    console.log('In add like');
+    numberLikes++
+    displayLikes();
+   return numberLikes;
+}
+function removeLike(){
+    console.log('In remove Like');
+    if (numberLikes <= 0) {
+         numberLikes = 0;
+    }else{
+        numberLikes--
+    }
+    displayLikes();
+    return numberLikes;
+}
+
+function displayLikes() {
+    $('#displayNumLikesForPost').empty();
+    $('#displayNumLikesForPost').append(`${numberLikes}`);
+    
+}
